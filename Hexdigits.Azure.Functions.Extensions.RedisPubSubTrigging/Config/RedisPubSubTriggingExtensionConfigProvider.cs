668fcb3a96ed07d8f07ca05f4e751c485a07c739
Dino Wang <dino.cool@gmail.com>
2019-03-06 09:40:43 +0800

    impl interface

--- Hexdigits.Azure.Functions.Extensions.RedisPubSubTrigging/Config/RedisPubSubTriggingExtensionConfigProvider.cs
+++ Hexdigits.Azure.Functions.Extensions.RedisPubSubTrigging/Config/RedisPubSubTriggingExtensionConfigProvider.cs
@@ -1,13 +1,14 @@
 using Microsoft.Azure.WebJobs.Description;
 using Microsoft.Azure.WebJobs.Host.Config;
 using StackExchange.Redis;
-using Hexdigits.Azure.Functions.Extensions.RedisClientFactory.Binding;
 
 namespace Hexdigits.Azure.Functions.Extensions.RedisPubSubTrigging.Config
 {
     [Extension("RedisPubSubTrigging")]
     internal class RedisPubSubTriggingExtensionConfigProvider : IExtensionConfigProvider
     {
-        
+        public void Initialize(ExtensionConfigContext context)
+        {
+        }
     }
 }
